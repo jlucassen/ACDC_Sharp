e870dfc3364caff8ece4ab4f21527eefadf48d28
James <james.z.lucassen@gmail.com>
2024-01-29 15:07:36 +0000

    graphviz node positioning sucks

--- toy_circuits/toy_circuits.py
+++ toy_circuits/toy_circuits.py
@@ -8,6 +8,9 @@ class Node:
         self.id = id
         self.gate = gate
         self.children = children
+        self.layer = 0
+        if isinstance(children, list):
+            self.layer = 1 + max([child.layer for child in self.children])
         
     def forward(self):
         if self.gate == 'AND':
@@ -61,14 +64,23 @@ def make_tree(num_gates:int, num_inputs:int, fan_in:int,  input_value:bool):
         count += 1
     return graph
 
-def visualize_graph(graph:list):
-    viz = graphviz.Digraph()
+def visualize_graph(graph:list, filename:str='temp.gv'):
+    def count_to_pos(count:int):
+        return count * (-1 if count%2==0 else 1)
+
+    viz = graphviz.Digraph(engine='dot')
     color_code = {'INPUT':'black', 'AND':'red', 'OR':'blue'}
+    layer_counts = [0]*len(graph)
     for node in graph:
-        viz.node(str(node.id), color=color_code[node.gate])
         if node.gate != 'INPUT':
+            viz.node(str(node.id), color=color_code[node.gate])#, pos=f"{count_to_pos(layer_counts[node.layer])},{node.layer}!")
+            layer_counts[node.layer] += 1
             for child in node.children:
                 viz.edge(str(child.id), str(node.id))
-    viz.render('doctest-output/round-table.gv')
+        else:
+            viz.node(str(node.id), color=color_code[node.gate])#, pos=f"{count_to_pos(layer_counts[node.layer])},{node.layer}!")
+            layer_counts[node.layer] += 1
+    viz.render(f'toy_circuits/circuit_viz/{filename}')
 
-visualize_graph(make_tree(2, 3, 2, False))
+visualize_graph(make_tree(15, 16, 2, False), 'demo-tree.gv')
+visualize_graph(make_dag(32, 16, 2, False), 'demo-dag.gv')
