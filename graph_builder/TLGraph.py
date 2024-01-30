fb9771bffe562bff9d4175bd9c6c0f046ea6e3aa
Carl Guo <carlguo866@gmail.com>
2024-01-30 13:56:04 +0000

    graph builder for attn only works

---
+++ graph_builder/TLGraph.py
@@ -0,0 +1,71 @@
+from typing import Tuple, Optional, Dict, Union, Set
+from transformer_lens import utils, HookedTransformer
+from collections import defaultdict
+from graphlib import TopologicalSorter
+
+def topological_sort(graph: Dict[Tuple[str, Optional[int]], Set[Tuple[str, Optional[int]]]]):
+    topo = TopologicalSorter(graph)
+    return list(topo.static_order())
+    
+    
+    
+def get_node_key(hook_name: str, head_idx: Optional[int] = None) -> Tuple[str, Optional[int]]:
+    if head_idx is None: 
+        return (hook_name, None)
+    else: 
+        return (hook_name, head_idx)
+
+
+def build_graph_from_model(model: HookedTransformer) -> Dict[Tuple[str, Optional[int]], 
+                                                             Set[Tuple[str, Optional[int]]]]: 
+    #keys are tuples that either is (layer_idx, head_idx) or (layer_idx, None)
+    
+    # setup
+    graph = defaultdict(set)
+    n_layers = model.cfg.n_layers
+    downstream_resid_nodes = set() # downstream means later in the forward pass
+    
+    # start with the root node (last resid node)
+    root_node = get_node_key(utils.get_act_name("resid_post", n_layers-1))
+    downstream_resid_nodes.add(root_node)
+    
+    for layer_idx in range(n_layers - 1, -1, -1):
+
+        # not supporting mlps right now 
+        if not model.cfg.attn_only: 
+            raise Exception("really need to implement this")
+        new_downstream_resid_nodes = set()
+        for head_idx in range(model.cfg.n_heads - 1, -1, -1):
+            head_name = f"blocks.{layer_idx}.attn.hook_result"
+            cur_node = get_node_key(head_name, head_idx)
+            for resid_node in downstream_resid_nodes: 
+                graph[cur_node].add(resid_node)
+            
+            # if model.cfg.use_split_qkv_input:
+            for decomposed_name in ["q", "k", "v"]: 
+                decomposed_node = get_node_key(
+                                                utils.get_act_name(decomposed_name, layer_idx), 
+                                                head_idx)
+                decomposed_input_node = get_node_key(
+                                                utils.get_act_name(f"{decomposed_name}_input", layer_idx), 
+                                                head_idx)
+
+                graph[decomposed_node].add(cur_node)
+                graph[decomposed_input_node].add(decomposed_node)
+                new_downstream_resid_nodes.add(decomposed_input_node)
+        downstream_resid_nodes.update(new_downstream_resid_nodes)
+                    
+                
+    # maybe implement no pos embed later
+    
+    tok_embed_node = get_node_key(utils.get_act_name("embed"))
+    pos_embed_node = get_node_key(utils.get_act_name("pos_embed"))
+    embed_nodes = [tok_embed_node, pos_embed_node]
+    for embed_node in embed_nodes: 
+        for resid_node in downstream_resid_nodes: 
+            graph[embed_node].add(resid_node)
+
+    return graph
+
+if __name__ == "__main__": 
+    pass 
---
+++ graph_playground.py
@@ -0,0 +1,106 @@
+#%% 
+from IPython import get_ipython
+ipython = get_ipython()
+if ipython is not None:
+    ipython.magic("%load_ext autoreload")
+    ipython.magic("%autoreload 2")
+
+
+import os; os.environ["ACCELERATE_DISABLE_RICH"] = "1"
+import sys
+from pathlib import Path
+import torch as t
+from torch import Tensor
+import numpy as np
+import einops
+from tqdm.notebook import tqdm
+import plotly.express as px
+import webbrowser
+import re
+import itertools
+from jaxtyping import Float, Int, Bool
+from typing import List, Optional, Callable, Tuple, Dict, Literal, Set, Union
+from functools import partial
+from IPython.display import display, HTML
+from rich.table import Table, Column
+from rich import print as rprint
+import circuitsvis as cv
+from pathlib import Path
+from transformer_lens.hook_points import HookPoint
+from transformer_lens import utils, HookedTransformer, ActivationCache
+from transformer_lens.components import Embed, Unembed, LayerNorm, MLP
+from graph_builder.TLGraph import *
+#%% 
+t.set_grad_enabled(False)
+
+if t.cuda.is_available():
+    device = t.device("cuda")
+elif t.backends.mps.is_available():
+    device = t.device("mps")
+else:
+    device = t.device("cpu")
+
+
+MAIN = __name__ == "__main__"
+
+
+# %%
+model = HookedTransformer.from_pretrained(
+    "gpt2-small",
+    center_unembed=True,
+    center_writing_weights=True,
+    fold_ln=True,
+    refactor_factored_attn_matrices=True,
+    device=device
+)
+
+#%% 
+
+prompt_format = [
+    "When John and Mary went to the shops,{} gave the bag to",
+    "When Tom and James went to the park,{} gave the ball to",
+    "When Dan and Sid went to the shops,{} gave an apple to",
+    "After Martin and Amy went to the park,{} gave a drink to",
+]
+name_pairs = [
+    (" Mary", " John"),
+    (" Tom", " James"),
+    (" Dan", " Sid"),
+    (" Martin", " Amy"),
+]
+
+# Define 8 prompts, in 4 groups of 2 (with adjacent prompts having answers swapped)
+prompts = [
+    prompt.format(name)
+    for (prompt, names) in zip(prompt_format, name_pairs) for name in names[::-1]
+]
+
+tokens = model.to_tokens(prompts, prepend_bos=True)
+# Move the tokens to the GPU
+tokens = tokens.to(device)
+# Run the model and cache all activations
+original_logits, cache = model.run_with_cache(tokens)
+
+#%% 
+act_name = utils.get_act_name("resid_post", 0)
+cache[act_name].shape, cache.accumulated_resid().shape
+
+#%% 
+for key in list(cache.keys()): 
+    print(key)
+# %%
+model = HookedTransformer.from_pretrained( 
+                                          "attn-only-2l", 
+                                            center_unembed=True,
+                                            center_writing_weights=True,
+                                            fold_ln=True,
+                                            refactor_factored_attn_matrices=True,
+                                            device=device)
+#%% 
+print("model.cfg.use_split_qkv_input", model.cfg.use_split_qkv_input)
+graph = build_graph_from_model(model)
+for key, value in graph.items(): 
+    print(key, value)
+#%% 
+topo_order = topological_sort(graph)
+
