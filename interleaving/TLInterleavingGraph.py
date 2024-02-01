f55bcfd6f566c59abc1629cd0075cb5b40ad9986
james.z.lucassen@gmail.com <carlguo866@gmail.com>
2024-02-01 16:58:46 +0000

    first stab of interleaving

---
+++ interleaving/TLInterleavingGraph.py
@@ -0,0 +1,213 @@
+from typing import Tuple, Optional, Dict, Union, Set
+from transformer_lens import utils, HookedTransformer
+from collections import defaultdict
+from graphlib import TopologicalSorter
+from enum import Enum
+
+class TLNodeIndex:
+    name: str 
+    index: Optional[int]
+    def __init__(self, name: str, index: Optional[int] = None):
+        self.name = name
+        self.index = index
+        self.grad = None
+        self.score = None
+    
+    def __eq__(self, other):
+        assert isinstance(other, TLNodeIndex)
+        return self.name == other.name and self.index == other.index
+    
+    def __repr__(self):
+        if self.index is None:
+            return self.name
+        return f"{self.name}[{self.index}] score:{self.score}"
+    
+    def __hash__(self):
+        return hash(self.__repr__())    
+    
+    def __gt__(self, other):
+        assert isinstance(other, TLNodeIndex)
+        if self.name == other.name: 
+            return self.index > other.index
+        else:
+            return self.name < other.name
+    
+    def torchlike_index(self):
+        if self.index is None:
+            return slice(None)
+        else: 
+            return tuple([slice(None)] * 2 + [self.index])
+    
+    
+class TLEdgeType(Enum):
+    ADDITION = 0
+    DIRECT_COMPUTATION = 1
+    PLACEHOLDER = 2
+    
+    def __eq__(self, other):
+        """Necessary because of extremely frustrating error that arises with load_ext autoreload (because this uses importlib under the hood: https://stackoverflow.com/questions/66458864/enum-comparison-become-false-after-reloading-module)"""
+
+        if type(self).__qualname__ != type(other).__qualname__:
+            return NotImplemented
+        return self.name == other.name and self.value == other.value
+    
+    
+# class TLEdge:
+#     type: TLEdgeType
+#     use_clean_grad: bool
+#     visited: bool
+#     present: bool 
+    
+#     def __init__(self, type: TLEdgeType):
+#         self.type = type
+#         self.use_clean_grad = None
+#         self.present = True
+#         self.clean_grad = None 
+#         self.corrupt_grad = None
+         
+    
+
+def get_incoming_edge_type(child_node: TLNodeIndex) -> TLEdgeType:
+    # parent_layer, parent_head = parent_node
+    child_layer = child_node.name
+    
+    if child_layer.endswith("result") or child_layer.endswith("z") or child_layer.endswith("mlp_out"):
+        return TLEdgeType.PLACEHOLDER
+    elif child_layer.endswith("resid_post") or child_layer.endswith("_input"):
+        return TLEdgeType.ADDITION
+    else:
+        return TLEdgeType.DIRECT_COMPUTATION
+    
+    
+class TLGraph():
+    
+    graph: Dict[TLNodeIndex, Set[TLNodeIndex]]
+
+    def __init__(self, model: HookedTransformer, use_pos_embed=False) -> None:
+        self.graph = defaultdict(set)
+        self.reverse_graph = defaultdict(set)
+        self.cfg = model.cfg
+        self.use_pos_embed = use_pos_embed
+        self.build_graph()
+        self.build_reverse_graph()
+        # self.build_edges()
+        self.topological_sort()
+        self.reverse_topo_order = self.topo_order[::-1]
+        
+        
+    def build_graph(self) -> None:
+        n_layers = self.cfg.n_layers
+        downstream_resid_nodes = set() # downstream means later in the forward pass
+    
+        # start with the root node (last resid node)
+        root_node = TLNodeIndex(utils.get_act_name("resid_post", n_layers-1))
+        downstream_resid_nodes.add(root_node)
+        
+        for layer_idx in range(n_layers - 1, -1, -1):
+
+            # not supporting mlps right now 
+            if not self.cfg.attn_only: 
+                mlp_out_node = TLNodeIndex(f"blocks.{layer_idx}.hook_mlp_out")
+                mlp_in_node = TLNodeIndex(f"blocks.{layer_idx}.hook_mlp_in")
+                for resid_node in downstream_resid_nodes: 
+                    self.graph[mlp_out_node].add(resid_node)
+                self.graph[mlp_in_node].add(mlp_out_node)
+                downstream_resid_nodes.add(mlp_in_node)
+                
+                
+                
+            new_downstream_resid_nodes = set()
+            for head_idx in range(self.cfg.n_heads - 1, -1, -1):
+                if self.cfg.use_attn_result: 
+                    head_name = f"blocks.{layer_idx}.attn.hook_result"
+                else: 
+                    head_name = utils.get_act_name("z", layer_idx)
+                cur_node = TLNodeIndex(head_name, head_idx)
+                for resid_node in downstream_resid_nodes: 
+                    self.graph[cur_node].add(resid_node)
+                
+                # if model.cfg.use_split_qkv_input:
+                for decomposed_name in ["q", "k", "v"]: 
+                    decomposed_node = TLNodeIndex(
+                                                    utils.get_act_name(decomposed_name, layer_idx), 
+                                                    head_idx)
+                    decomposed_input_node = TLNodeIndex(
+                                                    utils.get_act_name(f"{decomposed_name}_input", layer_idx), 
+                                                    head_idx)
+
+                    self.graph[decomposed_node].add(cur_node)
+                    self.graph[decomposed_input_node].add(decomposed_node)
+                    new_downstream_resid_nodes.add(decomposed_input_node)
+            downstream_resid_nodes.update(new_downstream_resid_nodes)
+                        
+        if self.use_pos_embed:
+            tok_embed_node = TLNodeIndex(utils.get_act_name("embed"))
+            pos_embed_node = TLNodeIndex(utils.get_act_name("pos_embed"))
+            embed_nodes = [tok_embed_node, pos_embed_node]
+            for embed_node in embed_nodes: 
+                for resid_node in downstream_resid_nodes: 
+                    self.graph[embed_node].add(resid_node)
+        else: 
+            resid_pre = TLNodeIndex(utils.get_act_name("resid_pre", 0))
+            for resid_node in downstream_resid_nodes: 
+                self.graph[resid_pre].add(resid_node)
+            
+                
+    def topological_sort(self):
+        topo = TopologicalSorter(self.graph)
+        self.topo_order = list(topo.static_order())
+        return self.topo_order
+    
+    def build_reverse_graph(self):
+        for node in self.graph: 
+            for child in self.graph[node]: 
+                self.reverse_graph[child].add(node)
+                
+                
+    # def build_edges(self):
+    #     self.edges = defaultdict(defaultdict(TLEdge))
+    #     for parent in self.graph:
+    #         for child in self.graph[parent]:
+    #             edge_type = get_incoming_edge_type(child)
+    #             self.edges[parent][child] = TLEdge(edge_type, True)
+        
+        
+    def __getitem__(self, node: TLNodeIndex):
+        return self.graph[node]
+    
+    def add_edge(self, sender: TLNodeIndex, receiver: TLNodeIndex):
+        self.graph[sender].add(receiver)
+        self.reverse_graph[receiver].add(sender)
+    
+    def remove_edge(self, sender: TLNodeIndex, receiver: TLNodeIndex):
+        self.graph[sender].remove(receiver)
+        self.reverse_graph[receiver].remove(sender)
+    
+    def count_edges(self): 
+        return sum([len(self.reverse_graph[node]) \
+                    if get_incoming_edge_type(node) != TLEdgeType.PLACEHOLDER \
+                    else 0 \
+                    for node in self.reverse_graph])
+    
+    def node_disconnected(self, node: TLNodeIndex):
+        return len(self.graph[node]) == 0
+    
+    def remove_node(self, node: TLNodeIndex):
+        all_parents = self.reverse_graph[node].copy() 
+        for parent in all_parents:
+            self.remove_edge(parent, node)
+            
+    def generate_reduced_reverse_graph(self):
+        reduced_graph = {}
+        for node in self.reverse_graph: 
+            if self.reverse_graph[node]:
+                reduced_graph[node] = self.reverse_graph[node]
+            
+        return reduced_graph
+            
+    
+if __name__ == "__main__":    
+    node_index = TLNodeIndex("test", 1)
+    print(node_index.torchlike_index())
+    node_index = TLNodeIndex("test")
+    print(node_index.torchlike_index())
--- interleaving/interleaving.py
+++ interleaving/interleaving.py
@@ -1,5 +1,6 @@
 # %%
 import torch as t
+from torch import Tensor
 from transformer_lens import HookedTransformer
 from ioi_dataset import IOIDataset
 from functools import partial
@@ -7,7 +8,8 @@ from tqdm import tqdm
 from collections import defaultdict
 import einops
 
-from acdc_reimpl.TLGraph import TLGraph, TLNodeIndex
+from TLInterleavingGraph import TLGraph, TLNodeIndex
+from jaxtyping import Float
 
 device = t.device('cuda') if t.cuda.is_available() else t.device('cpu')
 # %%
@@ -51,7 +53,7 @@ def get_activations_with_grads(model, dataset):
  cache_with_grads = {} 
 
  logits = model.run_with_hooks(
-    dataset.sentences,
+    dataset.toks,
     fwd_hooks = [(
      lambda s: True, # apply this hook everywhere
      partial(save_reference_hook, save_dict=cache_with_grads) # save references to cache_with_grads
@@ -60,51 +62,139 @@ def get_activations_with_grads(model, dataset):
 
  return logits, cache_with_grads
 
-clean_logits, clean_activations_with_grads = get_activations_with_grads(model, clean_dataset)
-corrupt_logits, corrupt_activations_with_grads = get_activations_with_grads(model, corrupt_dataset)
-# %%
-
+clean_logits, clean_cache = get_activations_with_grads(model, clean_dataset)
+corrupt_logits, corrupt_cache = get_activations_with_grads(model, corrupt_dataset)
 # calculate activation diff
-
-# %%
-
-threshold = 0
-
-metric_value.backward()
-
-graph = TLGraph(model)
-graph.reverse_graph[TLNodeIndex('blocks.11.hook_resid_post')]
-
-included_grads_to_metric = defaultdict([]) # lists of grads to metric along included path, keyed by receiver name
-included_grads_to_metric[graph.topo_order[0]] = list(t.autograd.grad(outputs=metric_value, inputs=clean_logits),
-                                                    t.autograd.grad(outputs=metric_value, inputs=corrupt_logits))
-
-for receiver in graph.topo_order:
- if receiver in included_grads_to_metric.keys(): # check if the recieving node has any included grads to metric
-  grads_to_metric = included_grads_to_metric[receiver]
- else: # if not, ignore this receiver
-  continue
- for sender in graph.reverse_graph[receiver]:
-  clean_sender_activation = clean_activations_with_grads[sender.name][sender.torchlike_index()]
-  corrupt_sender_activation = corrupt_activations_with_grads[sender.name][sender.torchlike_index()]
-  sender_activation_diff = clean_sender_activation - corrupt_sender_activation
-
-  clean_receiver_activation = clean_activations_with_grads[receiver.name][sender.torchlike_index()]
-  corrupt_receiver_activation = corrupt_activations_with_grads[receiver.name][sender.torchlike_index()]
-  clean_grad = t.autograd.grad(outputs=clean_receiver_activation, inputs=clean_sender_activation, retain_graph=True)
-  corrupt_grad = t.autograd.grad(outputs=corrupt_receiver_activation, inputs=corrupt_sender_activation, retain_graph=True)
+def ave_logit_diff(
+    logits: Float[Tensor, 'batch seq d_vocab'],
+    ioi_dataset: IOIDataset,
+    per_prompt: bool = False
+):
+    '''
+        Return average logit difference between correct and incorrect answers
+    '''
+    # Get logits for indirect objects
+    batch_size = logits.size(0)
+    io_logits = logits[range(batch_size), ioi_dataset.word_idx['end'][:batch_size], ioi_dataset.io_tokenIDs[:batch_size]]
+    s_logits = logits[range(batch_size), ioi_dataset.word_idx['end'][:batch_size], ioi_dataset.s_tokenIDs[:batch_size]]
+    # Get logits for subject
+    logit_diff = io_logits - s_logits
+    return logit_diff if per_prompt else logit_diff.mean()
   
-  clean_reciever_activation_diff = einops.einsum(sender_activation_diff, clean_grad, 'a b, b c -> a c')
-  corrupt_reciever_activation_diff = einops.einsum(sender_activation_diff, corrupt_grad, 'a b, b c -> a c')
+clean_logit_diff = ave_logit_diff(clean_logits, clean_dataset)
+corrupt_logit_diff = ave_logit_diff(corrupt_logits, corrupt_dataset)
 
-  for grad_to_metric in grads_to_metric:
-    clean_metric_diff = t.dot(clean_reciever_activation_diff, grad_to_metric) # sender diff is clean-corrupt, so this gives metric improvement
-    corrupt_metric_diff = t.dot(corrupt_reciever_activation_diff, grad_to_metric)
+def ioi_metric(
+    logits: Float[Tensor, "batch seq_len d_vocab"],
+    corrupted_logit_diff: float = corrupt_logit_diff,
+    clean_logit_diff: float = clean_logit_diff,
+    ioi_dataset: IOIDataset = clean_dataset
+ ):
+    patched_logit_diff = ave_logit_diff(logits, ioi_dataset)
+    return (patched_logit_diff - corrupted_logit_diff) / (clean_logit_diff - corrupted_logit_diff)
 
-    if clean_metric_diff > threshold:
-     included_grads_to_metric[sender].append(clean_grad @ grad_to_metric)
-    elif corrupt_metric_diff > threshold:
-     included_grads_to_metric[sender].append(corrupt_grad @ grad_to_metric)
+def negative_ioi_metric(logits: Float[Tensor, "batch seq_len d_vocab"]):
+    return -ioi_metric(logits)
+    
+# Get clean and corrupt logit differences
+clean_metric = ioi_metric(clean_logits, corrupt_logit_diff, clean_logit_diff, clean_dataset)
+corrupt_metric = ioi_metric(corrupt_logits, corrupt_logit_diff, clean_logit_diff, corrupt_dataset)
+
+print(f'Clean direction: {clean_logit_diff}, Corrupt direction: {corrupt_logit_diff}')
+print(f'Clean metric: {clean_metric}, Corrupt metric: {corrupt_metric}')
+
+#%% 
+# clean_logit_diff.backward()
+# corrupt_logit_diff.backward()
+# %%
+# threshold = 0
+graph = TLGraph(model)
+
+end_node = graph.topo_order[0]
+end_node_clean_act = clean_cache[end_node.name]
+end_node_corrupt_act = corrupt_cache[end_node.name]
+
+print(end_node_clean_act.requires_grad, clean_logit_diff.requires_grad)
+clean_grad = t.autograd.grad(outputs=clean_logit_diff, inputs=end_node_clean_act, retain_graph=True)[0]
+corrupt_grad = t.autograd.grad(outputs=corrupt_logit_diff, inputs=end_node_corrupt_act, retain_graph=True)[0]
+
+
+#%% 
+print(clean_grad.shape)
+
+
+end_node.grad = t.max(corrupt_logit_diff * clean_grad, clean_logit_diff * corrupt_grad)
+print()
+#%% 
+for receiver in graph.topo_order:
+    children = graph.reverse_graph[receiver]
+    for sender in children:
+        # edge = graph.edges[sender][receiver]
+        # if edge.type == TLEdgeType.PLACEHOLDER:
+        #     continue
+        # if edge.visited: 
+        #     continue
+        # edge.visited = True
+        
+        clean_sender_act = clean_cache[sender.name][sender.torchlike_index()]
+        corrupt_sender_act = corrupt_cache[sender.name][sender.torchlike_index()]
+        
+        clean_receiver_act = clean_cache[receiver.name][receiver.torchlike_index()]
+        corrupt_receiver_act = corrupt_cache[receiver.name][receiver.torchlike_index()]
+        
+        clean_grad = t.autograd.grad(outputs=clean_receiver_act, inputs=clean_sender_act, retain_graph=True)
+        corrupt_grad = t.autograd.grad(outputs=corrupt_receiver_act, inputs=corrupt_sender_act, retain_graph=True)
+        
+        clean_corrupt_diff = clean_sender_act - corrupt_sender_act
+        corrupt_clean_diff = corrupt_sender_act - clean_sender_act
+        
+        running_corrupt_grad_score = clean_corrupt_diff @ corrupt_grad @ receiver.grad
+        running_clean_grad_score = corrupt_clean_diff @ clean_grad @ receiver.grad
+        
+        if sender.grad is None: 
+          if running_corrupt_grad_score > running_clean_grad_score:
+            sender.grad = corrupt_grad
+            sender.score = running_corrupt_grad_score
+          else: 
+            sender.grad = -clean_grad
+            sender.score = running_clean_grad_score
+        else: 
+          if running_corrupt_grad_score > sender.score:
+            sender.grad = corrupt_grad
+            sender.score = running_corrupt_grad_score
+          elif running_clean_grad_score > sender.score:
+            sender.grad = -clean_grad
+            sender.score = running_clean_grad_score
+          
+#%% 
+        
+
+
+# #%% 
+
+#  if receiver in included_grads_to_metric.keys(): # check if the recieving node has any included grads to metric
+#   grads_to_metric = included_grads_to_metric[receiver]
+#  else: # if not, ignore this receiver
+#   continue
+#  for sender in graph.reverse_graph[receiver]:
+  
+
+#   clean_receiver_activation = clean_cache[receiver.name][sender.torchlike_index()]
+#   corrupt_receiver_activation = corrupt_activations_with_grads[receiver.name][sender.torchlike_index()]
+#   clean_grad = t.autograd.grad(outputs=clean_receiver_activation, inputs=clean_sender_activation, retain_graph=True)
+#   corrupt_grad = t.autograd.grad(outputs=corrupt_receiver_activation, inputs=corrupt_sender_activation, retain_graph=True)
+  
+#   clean_reciever_activation_diff = einops.einsum(sender_activation_diff, clean_grad, 'a b, b c -> a c')
+#   corrupt_reciever_activation_diff = einops.einsum(sender_activation_diff, corrupt_grad, 'a b, b c -> a c')
+
+#   for grad_to_metric in grads_to_metric:
+#     clean_metric_diff = t.dot(clean_reciever_activation_diff, grad_to_metric) # sender diff is clean-corrupt, so this gives metric improvement
+#     corrupt_metric_diff = t.dot(corrupt_reciever_activation_diff, grad_to_metric)
+
+#     if clean_metric_diff > threshold:
+#      included_grads_to_metric[sender].append(clean_grad @ grad_to_metric)
+#     elif corrupt_metric_diff > threshold:
+#      included_grads_to_metric[sender].append(corrupt_grad @ grad_to_metric)
      
 
 
