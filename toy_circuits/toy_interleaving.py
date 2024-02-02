391e9cb1132d666ed12c5c2df1d403e9c395e568
James <james.z.lucassen@gmail.com>
2024-02-02 14:39:55 +0000

    full frankenstein alg working, on trees at least

--- toy_circuits/toy_interleaving.py
+++ toy_circuits/toy_interleaving.py
@@ -1,23 +1,24 @@
 import graphviz 
 import toy_circuits as tc
 
-def visualize_graph_edges(graph:list, edges:dict={}, filename:str='temp.gv'):
+def visualize_graph_edges(graph:list, edge_batches:list=[{}], filename:str='temp.gv'):
     color_code = {'INPUT':'black', 'AND':'red', 'OR':'blue'}
-    viz = graphviz.Digraph(engine='dot')
-    for node in graph:
-        node_kwargs = {
-            'fillcolor':color_code[node.gate],
-            'style':'filled',
-            'fontcolor':'white',
-        }
-        viz.node(str(node.id), **node_kwargs)
-        for child in node.children:
-            edge_key = (str(child.id), str(node.id))
-            if edge_key in edges.keys():
-                viz.edge(str(child.id), str(node.id), **edges[edge_key]) # plot selected edges with corresponding kwargs
-            else:
-                viz.edge(str(child.id), str(node.id))
-    viz.render(f'toy_circuits/circuit_viz/{filename}')
+    for i, edges in enumerate(edge_batches):
+        viz = graphviz.Digraph(engine='dot')
+        for node in graph:
+            node_kwargs = {
+                'fillcolor':color_code[node.gate],
+                'style':'filled',
+                'fontcolor':'white',
+            }
+            viz.node(str(node.id), **node_kwargs)
+            for child in node.children:
+                edge_key = (str(child.id), str(node.id))
+                if edge_key in edges.keys():
+                    viz.edge(str(child.id), str(node.id), **edges[edge_key]) # plot selected edges with corresponding kwargs
+                else:
+                    viz.edge(str(child.id), str(node.id))
+        viz.render(f'toy_circuits/circuit_viz/{filename}_{i}')
 
 def get_edge_grad(node, child, noise):
     old_forward = node.forward()
@@ -43,13 +44,24 @@ def set_all_inputs(graph:list, value:bool=False):
         if node.gate == 'INPUT':
             node.value = value
 
-def frankenstein_path_algorithm(graph:list, n_iters:int=5):
+def recursive_add(node, edges, combined_edges):
+    nodes_to_add = []
+    for child in node.children:
+        edge_key = (str(child.id), str(node.id))
+        if edge_key in edges.keys():
+            nodes_to_add.append(child)
+            nodes_to_add += recursive_add(child, edges, combined_edges)
+            combined_edges[edge_key] = edges[edge_key]
+    return nodes_to_add
+
+def frankenstein_path_algorithm(graph:list, n_iters:int=5, greedy=False):
     noising_edges = get_all_edge_grads(graph, noise=True, color='red')
     set_all_inputs(graph, False)
     denoising_edges = get_all_edge_grads(graph, noise=False, color='blue')
 
     combined_edges = {}
     included_nodes = [graph[-1]]
+    edge_batches = []
     for _ in range(n_iters):
         nodes_to_include = []
         for node in included_nodes:
@@ -58,14 +70,19 @@ def frankenstein_path_algorithm(graph:list, n_iters:int=5):
                 if edge_key in noising_edges.keys():
                     combined_edges[edge_key] = noising_edges[edge_key]
                     nodes_to_include.append(child) # we have now found a path from child up to exit that has high grads all the way
+                    if greedy:
+                        nodes_to_include += recursive_add(child, noising_edges, combined_edges) # adds to combined edges internally
                     if edge_key in denoising_edges.keys():
                         raise NotImplementedError # don't know how to handle doubles yet
                 elif edge_key in denoising_edges:
                     combined_edges[edge_key] = denoising_edges[edge_key]
                     nodes_to_include.append(child) # we have now found a path from child up to exit that has high grads all the way
+                    if greedy:
+                        nodes_to_include += recursive_add(child, denoising_edges, combined_edges) # adds to combined edges internally
         included_nodes += nodes_to_include
         nodes_to_include = []
-    return combined_edges
+        edge_batches.append(combined_edges.copy())
+    return edge_batches
                 
 
 tree_size_param = 5
@@ -82,10 +99,10 @@ tree_on_denoise_edges = get_all_edge_grads(tree_on, noise=False, color='blue')
 tree_off_noise_edges = get_all_edge_grads(tree_off, noise=True)
 tree_off_denoise_edges = get_all_edge_grads(tree_off, noise=False, color='blue')
 
-visualize_graph_edges(tree_on, tree_on_noise_edges, 'tree_on_noise_edges.gv')
-visualize_graph_edges(tree_on, tree_on_denoise_edges, 'tree_on_denoise_edges.gv')
-visualize_graph_edges(tree_off, tree_off_noise_edges, 'tree_off_noise_edges.gv')
-visualize_graph_edges(tree_off, tree_off_denoise_edges, 'tree_off_denoise_edges.gv')
+visualize_graph_edges(tree_on, [tree_on_noise_edges], 'tree_on_noise_edges.gv')
+visualize_graph_edges(tree_on, [tree_on_denoise_edges], 'tree_on_denoise_edges.gv')
+visualize_graph_edges(tree_off, [tree_off_noise_edges], 'tree_off_noise_edges.gv')
+visualize_graph_edges(tree_off, [tree_off_denoise_edges], 'tree_off_denoise_edges.gv')
 
-tree_on_frankenstein_edges = frankenstein_path_algorithm(tree_on, n_iters=5)
+tree_on_frankenstein_edges = frankenstein_path_algorithm(tree_on, n_iters=5, greedy=True)
 visualize_graph_edges(tree_on, tree_on_frankenstein_edges)
